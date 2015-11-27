c92f7827a6b62664b80167f3388ac2ec9bef9027
stvno <github@minst.net>
2015-11-27 12:51:25 -0500

    ol3 version of cbsexplorer

---
+++ page/cbsexplorerol/src/main.js
@@ -0,0 +1,251 @@
+"use strict";
+//(function(){
+/*general map related code*/
+
+
+
+
+var createPopup = function(id) {
+    var attr= attributes.filter(function(d){return d.attr == c.attr})[0];
+    var feat = buurtById.get(id);
+    var innertext = '<h4>'+feat.gm_naam+', '+feat.bu_naam+'</h4>';
+    if(feat[c.attr]===null) {
+        innertext+='<span>There are no figures available for this place and theme</span>';
+    }
+    else innertext+='<span>'+attr.label+': '+feat[c.attr]+' '+attr.unit+'</span>';
+    
+    
+    return innertext;
+}
+/* classification related code*/
+var linewidth = 16/18;
+//map.on('zoomend',function(){linewidth=map.getZoom()/18})
+var buurtById = d3.map();
+var tsvIsLoaded = false;
+//classification settings
+var c = {
+    color: 'OrRd',
+    cnt: 5,
+    type: 'q', //quantile 'q', linear 'l', sd-divergent 's'
+    attr: 'p_00_14_jr',
+    soort: 'p', //percentage 'p'
+    steps: [20,40,60,80],
+    range: 's' //sequential 's', divergent 'd'
+};
+var scale;
+
+var setScale = function() {
+
+    //get an array of the relevant values
+    var values = buurtById.values().map(function(d){return d[c.attr]});
+    //choose color and # classes;
+    var cb = colorbrewer[c.color][c.cnt];
+    //choose type of classification
+    switch(c.type) {
+        case 'q':
+            //quantile
+
+            scale = d3.scale.quantile()
+                .domain(values)
+                .range(cb)
+            c.steps = scale.quantiles();
+        break;
+        case 'l':
+            //linear
+            var min = d3.min(values);
+            var max = d3.max(values);
+            var step = (max-min)/(c.cnt-1);
+            c.steps = [];
+            for(var i = 1;i<c.cnt;i++) {
+                c.steps.push(min+i*step);
+            }
+            scale = d3.scale.threshold()
+                .domain(c.steps)
+                .range(cb);
+            
+        break;
+        case 's':
+            //sd-divergent
+            var dev = d3.deviation(values);
+            var mean = d3.mean(values);
+            switch(c.cnt) {
+                case '3':
+                    c.steps = [mean-dev,mean+dev]
+                break;
+                case '4':
+                    c.steps = [mean-dev,mean,mean+dev]
+                break;
+                case '5':
+                    c.steps = [mean-2*dev,mean-dev,mean+dev,mean+2*dev]
+                break;
+                case '6':
+                    c.steps = [mean-2*dev,mean-dev,mean,mean+dev,mean+2*dev]
+                break;
+                case '7':
+                    c.steps = [mean-2*dev,mean-dev,mean-dev/2,mean+dev/2,mean+dev,mean+2*dev]
+                break;
+                case '8':
+                    c.steps = [mean-2*dev,mean-dev,mean-dev/2,mean,mean+dev/2,mean+dev,mean+2*dev]
+                break;
+                case '9':
+                    c.steps = [mean-2*dev,mean-1.5*dev,mean-dev,mean-dev/2,mean+dev/2,mean+dev,mean+1.5*dev,mean+2*dev]
+                break;
+            }
+            //TODO: land area adam-noord/broek delen door 0?
+            scale = d3.scale.threshold()
+                .domain(c.steps)
+                .range(cb)
+        break;
+    }
+}
+
+var colorMe = function(id) {
+    var fill = new ol.style.Fill({color: ''});
+    var stroke = new ol.style.Stroke({color: 'rgba(0,0,0,0.5)', width: 1});
+    var polygon = new ol.style.Style({fill: fill, stroke: stroke});    
+    var styles = [];
+    return function(feature,resolution) {
+        var id = feature.get('id');
+        
+    
+        if (scale === undefined){
+           
+        }
+        else if(buurtById.get(id).water == "NEE"&&buurtById.get(id)[c.attr]!==null) {
+            fill.setColor(scale(buurtById.get(id)[c.attr]));
+            styles[length++] = polygon;
+            
+        }
+        else {
+            fill.setColor('rgba(0,0,0,0)');
+            styles[length++] = polygon;            
+        }
+        styles.length = length;
+        return styles;
+    }
+}
+
+/*TSV related code*/
+d3.tsv('data/cbsbuurt.tsv',function(d) {
+    d.forEach(function(f){
+        var buurt = {};
+        buurt.a_bst_b = parseInt(f.a_bst_b)<0?null:parseInt(f.a_bst_b);
+        buurt.a_bst_nb = parseInt(f.a_bst_nb)<0?null:parseInt(f.a_bst_nb);
+        buurt.a_lftj6j = parseInt(f.a_lftj6j)<0?null:parseInt(f.a_lftj6j);
+        buurt.a_lfto6j = parseInt(f.a_lfto6j)<0?null:parseInt(f.a_lfto6j);
+        buurt.aant_inw =parseInt(f.aant_inw)<0?null:parseInt(f.aant_inw);
+        buurt.aant_man =parseInt(f.aant_man)<0?null:parseInt(f.aant_man);
+        buurt.aant_vrouw =parseInt(f.aant_vrouw)<0?null:parseInt(f.aant_vrouw);
+        buurt.aantal_hh =parseInt(f.aantal_hh)<0?null:parseInt(f.aantal_hh);
+        buurt.auto_hh =parseFloat(f.auto_hh)<0?null:parseFloat(f.auto_hh);
+        buurt.auto_land =parseInt(f.auto_land)<0?null:parseInt(f.auto_land);
+        buurt.auto_tot =parseInt(f.auto_tot)<0?null:parseInt(f.auto_tot);
+        buurt.bedr_auto =parseInt(f.bedr_auto)<0?null:parseInt(f.bedr_auto);
+        buurt.bev_dichth =parseInt(f.bev_dichth)<0?null:parseInt(f.bev_dichth);
+        buurt.bu_code =f.bu_code;
+        buurt.bu_naam =f.bu_naam;
+        buurt.gem_hh_gr =parseFloat(f.gem_hh_gr)<0?null:parseFloat(f.gem_hh_gr);
+        buurt.gm_code =f.gm_code;
+        buurt.gm_naam =f.gm_naam;
+        buurt.id =parseInt(f.id)<0?null:parseInt(f.id);
+        buurt.motor_2w =parseInt(f.motor_2w)<0?null:parseInt(f.motor_2w);
+        buurt.opp_land =parseInt(f.opp_land)<0?null:parseInt(f.opp_land);
+        buurt.opp_tot =parseInt(f.opp_tot)<0?null:parseInt(f.opp_tot);
+        buurt.opp_water =parseInt(f.opp_water)<0?null:parseInt(f.opp_water);
+        buurt.p_00_14_jr =parseInt(f.p_00_14_jr)<0?null:parseInt(f.p_00_14_jr);
+        buurt.p_15_24_jr =parseInt(f.p_15_24_jr)<0?null:parseInt(f.p_15_24_jr);
+        buurt.p_25_44_jr =parseInt(f.p_25_44_jr)<0?null:parseInt(f.p_25_44_jr);
+        buurt.p_45_64_jr =parseInt(f.p_45_64_jr)<0?null:parseInt(f.p_45_64_jr);
+        buurt.p_65_eo_jr =parseInt(f.p_65_eo_jr)<0?null:parseInt(f.p_65_eo_jr);
+        buurt.p_ant_aru =parseInt(f.p_ant_aru)<0?null:parseInt(f.p_ant_aru);
+        buurt.p_eenp_hh =parseInt(f.p_eenp_hh)<0?null:parseInt(f.p_eenp_hh);
+        buurt.p_gehuwd =parseInt(f.p_gehuwd)<0?null:parseInt(f.p_gehuwd);
+        buurt.p_gescheid =parseInt(f.p_gescheid)<0?null:parseInt(f.p_gescheid);
+        buurt.p_hh_m_k =parseInt(f.p_hh_m_k)<0?null:parseInt(f.p_hh_m_k);
+        buurt.p_hh_z_k =parseInt(f.p_hh_z_k)<0?null:parseInt(f.p_hh_z_k);
+        buurt.p_marokko =parseInt(f.p_marokko)<0?null:parseInt(f.p_marokko);
+        buurt.p_n_w_al =parseInt(f.p_n_w_al)<0?null:parseInt(f.p_n_w_al);
+        buurt.p_ongehuwd =parseInt(f.p_ongehuwd)<0?null:parseInt(f.p_ongehuwd);
+        buurt.p_over_nw =parseInt(f.p_over_nw)<0?null:parseInt(f.p_over_nw);
+        buurt.p_surinam =parseInt(f.p_surinam)<0?null:parseInt(f.p_surinam);
+        buurt.p_turkije =parseInt(f.p_turkije)<0?null:parseInt(f.p_turkije);
+        buurt.p_verweduw =parseInt(f.p_verweduw)<0?null:parseInt(f.p_verweduw);
+        buurt.p_west_al =parseInt(f.p_west_al)<0?null:parseInt(f.p_west_al);
+        buurt.water =f.water;
+        buurt.wk_code =f.wk_code;
+        buurtById.set(parseInt(f.id), buurt)
+    });
+    tsvLoaded();
+});
+//TSV is loaded, stop the tsv-spinner
+
+var tsvLoaded = function() {
+    document.getElementById('spinnerdiv').style.display='none';
+    document.getElementById('spinner').classList.remove('cssload-whirlpool');
+    tsvIsLoaded = true;
+    refreshStyle();
+};
+
+/* MVT related code*/
+var style = function(f) {
+    var style = {};
+    if(tsvIsLoaded) {
+        style.color = colorMe(f.properties.id);
+        style.outline = {
+            color:  'rgba(0,0,0,0.5)',
+            size: linewidth
+        };
+    }
+    else {
+        style.color = 'rgba(197,27,138,0.2)';
+        style.outline = {
+                color: 'rgb(122,1,119)',
+                size: linewidth
+        };
+    }
+    return style;
+}
+var refreshStyle = function(newC) {
+    if(newC!==undefined) {
+
+    }
+    setScale();
+//    mvtSource.setStyle(style);
+    createLegend();
+}
+
+
+/*// ADD THE REFERENCE OVERLAY (THE TOP OF THE SANDWICH) HERE:
+var topPane = L.DomUtil.create('div', 'leaflet-top-pane', map.getPanes().mapPane);
+var topLayer = new L.tileLayer('http://{s}.tile.stamen.com/toner-labels/{z}/{x}/{y}.png', {
+  maxZoom: 17
+}).addTo(map);
+topPane.appendChild(topLayer.getContainer());
+topLayer.setZIndex(3);*/
+//})();
+var map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+        source: new ol.source.Stamen({layer:'toner'})
+    }),
+    new ol.layer.VectorTile({
+      source: new ol.source.VectorTile({
+        attributions: [new ol.Attribution({
+          html: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> ' +
+              '© <a href="http://www.openstreetmap.org/copyright">' +
+              'OpenStreetMap contributors</a>'
+        })],
+        format: new ol.format.MVT(),
+        tileGrid: ol.tilegrid.createXYZ({maxZoom: 18}),
+        tilePixelRatio: 16,
+        url: "https://{a-c}.tiles.mapbox.com/v4/wherecampeu.33aym4j6/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1Ijoid2hlcmVjYW1wZXUiLCJhIjoieHE4bVNuRSJ9.qFTj9L2TMzVXX8G2QwJl_g"
+      }),
+      style: colorMe()
+    })
+  ],
+  target: 'map',
+  view: new ol.View({
+    center: ol.proj.fromLonLat([5, 52]),
+    zoom: 13
+  })
+});
---
+++ page/cbsexplorerol/src/ui.js
@@ -0,0 +1,196 @@
+'use strict';
+//(function(){
+var piefje = document.getElementById('piefje');
+var sidebar = document.getElementById('sidebar');
+piefje.onclick= function(e){
+    if(piefje.classList.contains('open')) {
+        piefje.classList.remove('open');
+        sidebar.classList.remove('open');
+    }
+    else {
+        piefje.classList.add('open');
+        sidebar.classList.add('open');
+    }
+};
+[].forEach.call(document.querySelectorAll('#sidebar input.classselect'), function (input) {
+    input['onclick' in input ? 'onclick' : 'onchange'] = function (e) {
+        c.type = this.value;
+        //TODO: get values from input
+        if(c.type=='s' && c.range !== 'd') { 
+            c.color = "PuOr";
+            c.range = 'd';
+        }
+        else if ((c.type=='l'||c.type=='q')&&c.range!=='s') {
+            c.color = "YlGn";
+            c.range = 's';
+        }        
+        refreshStyle(c);
+        refreshColors();
+    };
+});
+var selector = document.getElementById('themeselector');
+selector.onchange = function(e) {
+    c.attr = this.value;
+    refreshStyle(c);
+};
+var numberclass = document.getElementById('numberclasses');
+numberclass.onchange = function(e) {
+    c.cnt = this.value; 
+    refreshStyle();
+}
+var colors = [
+{name:'YlGn',type:'seq'},
+{name:'YlGnBu',type:'seq'},
+{name:'GnBu',type:'seq'},
+{name:'BuGn',type:'seq'},
+{name:'PuBuGn',type:'seq'},
+{name:'PuBu',type:'seq'},
+{name:'BuPu',type:'seq'},
+{name:'RdPu',type:'seq'},
+{name:'PuRd',type:'seq'},
+{name:'OrRd',type:'seq'},
+{name:'YlOrRd',type:'seq'},
+{name:'YlOrBr',type:'seq'},
+{name:'Purples',type:'seq'},
+{name:'Blues',type:'seq'},
+{name:'Greens',type:'seq'},
+{name:'Oranges',type:'seq'},
+{name:'Reds',type:'seq'},
+{name:'Greys',type:'seq'},
+{name:'PuOr',type:'div'},
+{name:'BrBG',type:'div'},
+{name:'PRGn',type:'div'},
+{name:'PiYG',type:'div'},
+{name:'RdBu',type:'div'},
+{name:'RdGy',type:'div'},
+{name:'RdYlBu',type:'div'},
+{name:'Spectral',type:'div'},
+{name:'RdYlGn',type:'div'}
+]
+var colorlist = document.getElementById('colorlist');
+var refreshColors = function() {
+    colorlist.innerHTML = null;
+    var showcolors = colors.filter(function(d){
+        if (c.type=='s') return d.type == 'div';
+        else return d.type=='seq';
+    })
+    showcolors.forEach(function(d){        
+        var t = document.querySelector('#colorTemplate');
+        t.content.querySelector('.colorbar').classList.remove('active');
+        var svg = t.content.querySelector('svg');
+        svg.id = d.name;
+        var c0 = t.content.querySelector('.c0');
+        var c1 = t.content.querySelector('.c1');
+        var c2 = t.content.querySelector('.c2');
+        c0.attributes.fill.value = colorbrewer[d.name][3][0];
+        c1.attributes.fill.value = colorbrewer[d.name][3][1];
+        c2.attributes.fill.value = colorbrewer[d.name][3][2];
+        if(d.name==c.color) t.content.querySelector('.colorbar').classList.add('active');
+        var clone = document.importNode(t.content, true);
+        colorlist.appendChild(clone);
+    });
+};
+refreshColors();
+colorlist.onclick = function(e) {
+    var svg = e.target.hasAttribute('y')?e.target.parentNode:e.target.children[0];
+    c.color = svg.id;
+    [].forEach.call(document.querySelectorAll('#sidebar .colorbar'), function (d) {
+        d.classList.remove('active')
+    });
+    svg.parentNode.classList.add('active');
+    refreshStyle(c);
+    }
+var attributes = [
+ {attr: 'p_00_14_jr', label: '% 0-14 year', type: 'percentage', unit :'%'},
+ {attr: 'p_15_24_jr', label: '% 15-24 year', type: 'percentage', unit :'%'},
+ {attr: 'p_25_44_jr', label: '% 25-44 year', type: 'percentage', unit :'%'},
+ {attr: 'p_45_64_jr', label: '% 45-64 year', type: 'percentage', unit :'%'},
+ {attr: 'p_65_eo_jr', label: '% 65+ year', type: 'percentage', unit :'%'},
+ {attr: 'p_ant_aru', label: '% from Antilles & Aruba', type: 'percentage', unit :'%'},
+ {attr: 'p_eenp_hh', label: '% Single person households', type: 'percentage', unit :'%'},
+ {attr: 'p_gehuwd', label: '% Maried people', type: 'percentage', unit :'%'},
+ {attr: 'p_gescheid', label: '% Divorced people', type: 'percentage', unit :'%'},
+ {attr: 'p_hh_m_k', label: '% Households with kids', type: 'percentage', unit :'%'},
+ {attr: 'p_hh_z_k', label: '% Households without kids', type: 'percentage', unit :'%'},
+ {attr: 'p_marokko', label: '% from Marocco', type: 'percentage', unit :'%'},
+ {attr: 'p_n_w_al', label: '% Non-western immigrants', type: 'percentage', unit :'%'},
+ {attr: 'p_ongehuwd', label: '% Unmaried people', type: 'percentage', unit :'%'},
+ {attr: 'p_over_nw', label: '% Other non-western immigrants', type: 'percentage', unit :'%'},
+ {attr: 'p_surinam', label: '% from Suriname', type: 'percentage', unit :'%'},
+ {attr: 'p_turkije', label: '% from Turkey', type: 'percentage', unit :'%'},
+ {attr: 'p_verweduw', label: '% Widow(er)s', type: 'percentage', unit :'%'},
+ {attr: 'p_west_al', label: '% Western immigrants', type: 'percentage', unit :'%'},
+ {attr: 'a_bst_b', label: '# Petrol cars', type: 'absolute', unit: 'cars'},
+ {attr: 'a_bst_nb', label: '# Non-petrol cars', type: 'absolute', unit: 'cars'},
+ {attr: 'a_lftj6j', label: '# Cars < 6 years', type: 'absolute', unit: 'cars'},
+ {attr: 'a_lfto6j', label: '# Cars 6+ years', type: 'absolute', unit: 'cars'},
+ {attr: 'aant_inw', label: '# Inhabitants', type: 'absolute', unit: 'persons'},
+ {attr: 'aant_man', label: '# Males', type: 'absolute', unit: 'males'},
+ {attr: 'aant_vrouw', label: '# Females', type: 'absolute', unit: 'females'},
+ {attr: 'aantal_hh', label: '# Households', type: 'absolute', unit: 'households'},
+ {attr: 'auto_hh', label: 'Cars/household', type: 'relative', unit: 'car/hh'},
+ {attr: 'auto_land', label: 'Car density', type: 'density', unit: 'car/km²'},
+ {attr: 'auto_tot', label: '# Cars', type: 'absolute', unit: 'cars'},
+ {attr: 'bedr_auto', label: '# Company cars', type: 'absolute', unit: 'cars'},
+ {attr: 'bev_dichth', label: 'Population density', type: 'density', unit: 'persons/km²'},
+ {attr: 'gem_hh_gr', label: 'Average household size', type: 'relative', unit: 'persons'},
+ {attr: 'motor_2w', label: '# Motorbikes', type: 'absolute', unit: 'bikes'},
+ {attr: 'opp_land', label: 'Land area', type: 'absolute', unit: 'ha'},
+ {attr: 'opp_tot', label: 'Total area', type: 'absolute', unit: 'ha'},
+ {attr: 'opp_water', label: 'Water area', type: 'absolute', unit: 'ha'}
+];
+
+attributes.forEach(function(d){
+    var t = document.querySelector('#optionTemplate');
+    var  o = t.content.querySelector('option');
+    o.value = d.attr;
+    o.innerHTML =d.label;
+    var clone = document.importNode(t.content, true);
+    selector.appendChild(clone);
+}) 
+
+var createLegend = function() {
+    var elLegend = document.querySelector('#legenda');
+    var attr = attributes.filter(function(d){return d.attr == c.attr});    
+    elLegend.innerHTML = '<h3>'+attr[0].label+'</h3>';
+    
+     var t = document.querySelector('#legendTemplate');
+        t.content.querySelector('.legendcolor').style.background = colorbrewer[c.color][c.cnt][0];
+        t.content.querySelector('.legendvalue').innerHTML =null;
+        t.content.querySelector('.legendunit').innerHTML =null;
+        var clone = document.importNode(t.content, true);
+        elLegend.appendChild(clone);
+    for(var i=1; i<c.steps.length+1; i++) {
+        var t = document.querySelector('#legendTemplate');
+        t.content.querySelector('.legendcolor').style.background = colorbrewer[c.color][c.cnt][i];
+        t.content.querySelector('.legendvalue').innerHTML =Math.round(c.steps[i-1]*100)/100;
+        t.content.querySelector('.legendunit').innerHTML =attr[0].unit;
+        
+        var clone = document.importNode(t.content, true);
+        elLegend.appendChild(clone);
+    }
+   
+}
+var opacityslider = document.getElementById('opacityslider');
+opacityslider.onchange = function(e) {
+    mvtSource.setOpacity(this.value)
+}
+var mettext = document.getElementById('mettext');
+mettext.onclick = function(e) {
+    if(e.target.classList.contains('show')){
+        e.target.classList.remove('show');
+        e.target.innerHTML = '?';
+    }
+    else {
+        e.target.classList.add('show');
+        e.target.innerHTML = '✔';
+    }
+     [].forEach.call(document.querySelectorAll('#sidebar .text'), function (d) {
+        d.classList.contains('hidden')?d.classList.remove('hidden'):d.classList.add('hidden');
+    });
+}
+var labelbox = document.getElementById('labels');
+labelbox.onclick = function(e) {
+    this.checked?topLayer.setOpacity(1):topLayer.setOpacity(0);
+}
+//})();
