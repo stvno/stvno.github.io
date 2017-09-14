1907dfe941074333d00324b231716ce76c74e6e9
Steven M. Ottens <github@minst.net>
2017-09-14 22:02:42 +0200

    added a menu

--- index.js
+++ index.js
@@ -39,6 +39,31 @@ function closePopup(e) {
 main.onclick = closePopup;
 close.onclick = closePopup;
 
+[].forEach.call(document.getElementsByClassName('thumb'), function (input) {
+    input['onclick' in input ? 'onclick' : 'ontouchend'] = function (e) {
+      e.stopPropagation();
+      var id = 'y'+e.target.innerText;
+      ScrollTo(id);
+      history.pushState(null, null, '#'+id);
+    };
+});
+
+
+function ScrollTo(name) {
+  ScrollToResolver(document.getElementById(name));
+}
+
+function ScrollToResolver(elem) {
+  var jump = parseInt(elem.getBoundingClientRect().top * .2);
+  document.body.scrollTop += jump;
+  document.documentElement.scrollTop += jump;
+  if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
+    elem.lastjump = Math.abs(jump);
+    setTimeout(function() { ScrollToResolver(elem);}, "100");
+  } else {
+    elem.lastjump = null;
+  }
+}
 })();
 
 
