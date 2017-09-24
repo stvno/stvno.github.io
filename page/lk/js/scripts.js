a45571d6b153b139f1f2af6b43db8e445972903a
Steven M. Ottens <github@minst.net>
2017-09-24 15:22:04 +0200

    mobile menu added

--- page/lk/js/scripts.js
+++ page/lk/js/scripts.js
@@ -1,1 +1,9 @@
-console.log('work in progress, @stvno')
+var menuMobile = document.getElementById('menu-mobile');
+var headerMobile = document.getElementById('header-mobile');
+menuMobile.onclick = function(e){
+	e.stopPropagation();
+	headerMobile.className = headerMobile.className == 'active' ? '': 'active';
+}
+headerMobile.onclick = function() {
+	headerMobile.className = '';
+}
