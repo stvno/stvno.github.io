965917d06dc00bfeeadcde0025f13ddcac9e2221
Steven M. Ottens <github@minst.net>
2017-10-16 11:26:26 +0200

    hunting a weird firefox bug

--- page/ahn/index.js
+++ page/ahn/index.js
@@ -114,6 +114,11 @@ hills.drawTile = function(canvas, tilePoint, zoom) {
     var y = Math.pow(2,zoom) - tilePoint.y -1;
     
     demImg.src= 'https://research.geodan.nl/service/ahn2/nlgrid/'+zoom+'/'+tilePoint.x+'/'+y+'.png';
+    if ( demImg.complete || demImg.complete === undefined ) {
+        demImg.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        demImg.src = src;
+        console.log(src)
+    }
 }
   
 
